1b195e2cf8e4f43976516f134da0141d931a3165
Sang Hyeon Jung <sh199919@naver.com>
2020-06-05 15:50:53 +0900

    1st commit

--- 체계적 12주차 과제/src/main/Main.java
+++ 체계적 12주차 과제/src/main/Main.java
@@ -3,6 +3,6 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("60181665 정상현 12주차 과제");
+		System.out.println("60181665 정상현 12주차 과제 입니다.");
 	}
 }
